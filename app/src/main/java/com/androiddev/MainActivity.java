b8ed203890df8d74f625c289d914ded9f85c650d
sean-mcgregor <worksmcg@gmail.com>
2021-02-21 19:58:03 +0200

    Added some location functionality and user request

--- app/src/main/java/com/androiddev/MainActivity.java
+++ app/src/main/java/com/androiddev/MainActivity.java
@@ -1,19 +1,36 @@
 package com.androiddev;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +43,24 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton startWalk = findViewById(R.id.startWalk);
         FloatingActionButton endWalk = findViewById(R.id.endWalk);
 
+        boolean sessionStarted = false;
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
         startWalk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Walk started!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
+                if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
+                    getLocation();
+                } else {
+
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+                }
             }
         });
 
@@ -43,6 +71,36 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
 
 
+            }
+        });
+    }
+
+    private void getLocation() {
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            getLocation();
+        } else {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+            @Override
+            public void onComplete(@NonNull Task<Location> task) {
+                Location location = task.getResult();
+                if (location != null) {
+                    try {
+
+                        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                        List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    } catch (IOException e){
+
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
---
+++ app/src/main/java/com/androiddev/WalkSession.java
@@ -0,0 +1,8 @@
+package com.androiddev;
+
+public class WalkSession {
+
+    public WalkSession(String [] logs){
+
+    }
+}
